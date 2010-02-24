6f54b75b012f730a774ae15378323bff3985ff2c
Alex Crichton <alex@alexcrichton.com>
2010-02-23 20:57:45 -0500

    Typo

--- app/models/create_song_job.rb
+++ app/models/create_song_job.rb
@@ -1,7 +1,7 @@
 class CreateSongJob < Struct.new(:file)
   
   def perform
-    raise "File didn't download!" unless File.exists?(File)
+    raise "File didn't download!" unless File.exists?(file)
     Song.create! :audio => File.open(file)
     File.delete file
   rescue => e
