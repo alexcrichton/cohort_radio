ead05a5358f8c80945f3f362e0342d5d4975344d
Alex Crichton <alex@alexcrichton.com>
2010-02-24 19:46:53 -0500

    Due to pool sizes, only allow one song to be enqueued at a time

--- lib/radio/fargo_daemon.rb
+++ lib/radio/fargo_daemon.rb
@@ -11,8 +11,15 @@ class Radio
       proxy = Radio::Proxy::FargoServer.new :client => client, :port => @port || DEFAULTS[:port]
       proxy.connect
       
+      @enqueue_lock = Mutex.new
+      
       client.subscribe { |type, hash|
-        Delayed::Job.enqueue CreateSongJob.new(hash[:file]) if type == :download_finished
+        if type == :download_finished
+          # Only enqueue one thing at a time. There was problems running into the connection
+          # pool running low as a result of many downloads finishing simultaneously. A pool
+          # of 10 ran out very quickly.
+          @enqueue_lock.synchronize{ Delayed::Job.enqueue CreateSongJob.new(hash[:file]) }
+        end
       }
 
       trap("INT")  { proxy.disconnect; client.disconnect; exit }
