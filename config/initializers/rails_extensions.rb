5c4b6ca048b47e49d620cb9deb140a8630862188
Alex Crichton <alex@alexcrichton.com>
2010-04-16 03:01:33 -0400

    Ugh I'm stupid

--- config/initializers/rails_extensions.rb
+++ config/initializers/rails_extensions.rb
@@ -2,5 +2,5 @@ require 'acts_as_slug'
 require 'radio'
 
 ActiveRecord::Base.class_eval { include Acts::Slug }
-ActionController::Base.class_eval { include Push::LongPolling }
+# ActionController::Base.class_eval { include Push::LongPolling }
 
