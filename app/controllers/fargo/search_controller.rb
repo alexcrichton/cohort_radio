96479c5430950dcd125cfddc746a3ab44be0be37
Alex Crichton <alex@alexcrichton.com>
2010-09-06 22:49:51 -0400

    Add capistrano tasks and don't by default filter to mp3's

--- app/controllers/fargo/search_controller.rb
+++ app/controllers/fargo/search_controller.rb
@@ -1,26 +1,30 @@
 class Fargo::SearchController < ApplicationController
-  
+
   before_filter { |c| c.authorize! :search, Fargo }
   before_filter :set_search
-  
+
   before_filter :require_fargo_connected
-    
+
   def index
     fargo.search @search if @search
   end
-  
+
   def results
     @results = fargo.search_results(@search) || []
 
     @result_map = Hash.new{ |h, k| h[k] = [] }
     @results.each { |result| @result_map[result[:nick]] << result }
   end
-  
+
   private
+
   def set_search
     return true unless params[:q]
-    params[:q] = "#{params[:q]} mp3" unless params[:q].index 'mp3'
-    @search = Fargo::Search.new(:query => params[:q], :filetype => Fargo::Search::AUDIO)
+
+    @search = Fargo::Search.new(
+      :query    => params[:q],
+      :filetype => Fargo::Search::AUDIO
+    )
   end
-    
+
 end
