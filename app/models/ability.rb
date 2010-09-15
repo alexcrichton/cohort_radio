b502eb2d67dcef6624b7af5c4e8f611d500921e5
Alex Crichton <alex@alexcrichton.com>
2010-09-14 22:34:18 -0400

    Allow rating of songs

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -1,11 +1,11 @@
 class Ability
   include CanCan::Ability
-  
+
   def initialize user, parent = nil
     alias_action :play_count, :to => :read
-    
+
     can :home, User
-      
+
     if user.nil? || user.activation.nil? || user.activation.pending?
       can :login, User
       can :create, User
@@ -17,32 +17,32 @@ class Ability
     elsif user.confirmed?
       can [:read, :create], Playlist
       can [:update, :add, :next, :stop], Playlist, :user_id => user.id
-      can [:download, :create, :search, :update], Song
+      can [:download, :create, :search, :update, :rate], Song
       can :manage, user
       can [:read, :logout], User
       can [:search, :download], Fargo
       can :read, :all
-      
+
       can :create, Comment
-      
+
       can :manage, Comment, :user_id => user.id
-      
+
       # can :create, QueueItem do |queue_item|
       #   parent.nil? || (parent.is_a?(Playlist) && can?(:add_to, parent.pool))
       # end
-      
+
       can :manage, Pool do |action, pool|
         !pool.playlist.private || pool.playlist.user_id == user.id || pool.playlist.user_ids.include?(user.id)
       end
-      
+
       can :destroy, QueueItem, :user_id => user.id
-      
+
       can [:create, :destroy], Membership do |membership|
         parent.is_a?(Playlist) && parent.user_id == user.id
       end
-      
+
       can :create, QueueItem
     end
-    
+
   end
 end
