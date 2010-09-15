69f7563f8c583f70cb01c02c1016d82d330a7398
Alex Crichton <alex@alexcrichton.com>
2010-09-15 01:48:29 -0400

    No longer using exceptional

--- app/models/create_song_job.rb
+++ app/models/create_song_job.rb
@@ -1,12 +1,9 @@
 class CreateSongJob < Struct.new(:file)
-  
+
   def perform
     raise "File didn't download!" unless File.exists?(file)
     Song.create! :audio => File.open(file)
     File.delete file
-  rescue => e
-    Exceptional.handle e
-    raise e
   end
-  
+
 end
