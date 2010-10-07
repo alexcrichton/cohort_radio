17b01718f169dc1669d854ad91a0d86ffea3fded
Alex Crichton <alex@alexcrichton.com>
2010-10-07 11:44:11 -0400

    Remove unneeded code

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,29 +11,7 @@ class User < ActiveRecord::Base
   validates_uniqueness_of :name, :case_sensitive => false
 
   attr_accessible :name, :email, :password, :password_confirmation
-  # has_one :activation, :dependent => :destroy
-  # after_create :create_activation
 
-  # scope :state, lambda{ |state| includes(:activation).where('activations.state = ?', state) }
   scope :admins, where(:admin => true)
   scope :search, Proc.new{ |query| where('name LIKE :q or email LIKE :q', :q => "%#{query}%") }
-
-  # def active?
-  #   activation && activation.activated?
-  # end
-  #
-  # def confirmed?
-  #   activation && activation.confirmed?
-  # end
-
-  # def deliver_activation_instructions!
-  #   Activation.create!(:state => 'pending', :user => self)
-  #   reset_perishable_token!
-  #   Notifier.send_later :deliver_activation_instructions, self
-  # end
-  #
-  # def deliver_password_reset_instructions!
-  #   reset_perishable_token!
-  #   Notifier.send_later :deliver_password_reset_instructions, self
-  # end
 end
