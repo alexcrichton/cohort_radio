1b5c7d8fc3fc7ced35a9fd73e4d0843c2ca6dbee
Alex Crichton <alex@alexcrichton.com>
2010-10-08 13:03:05 -0400

    Add a config directory for fargo

--- config/initializers/fargo.rb
+++ config/initializers/fargo.rb
@@ -1,6 +1,7 @@
 Fargo.configure do |config|
   config.nick         = 'cenphol2'
   config.download_dir = Rails.root.realpath.to_s + '/tmp/fargo/downloads'
+  config.config_dir   = Rails.root.realpath.to_s + '/tmp/fargo/config'
 end
 
 Fargo.logger.level = ActiveSupport::BufferedLogger::DEBUG
