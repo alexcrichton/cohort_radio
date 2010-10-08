9e6d2cf2ee911b856ea37009c79ea330f255993d
Alex Crichton <alex@alexcrichton.com>
2010-10-07 22:35:54 -0400

    Get the hostname to connect to from the window

--- app/javascripts/pipe.js
+++ app/javascripts/pipe.js
@@ -5,7 +5,7 @@ $.pipe = $('<div/>').hide();
 $(function() {
   $(document.body).append($.pipe);
 
-  var ws = new WebSocket('ws://localhost:8080');
+  var ws = new WebSocket('ws://' + window.location.hostname + ':8080');
   ws.onmessage = function(event) {
     $.pipe.trigger(JSON.parse(event.data));
   };
