a26cb6215f8134a2875df19fbb55aaa3830a4740
Alex Crichton <alex@alexcrichton.com>
2010-10-09 21:48:49 -0400

    Bind the websocket on 0.0.0.0 so we can have regular http clients connect to it in the future

--- config/initializers/fargo.rb
+++ config/initializers/fargo.rb
@@ -2,6 +2,7 @@ Fargo.configure do |config|
   config.nick         = 'cenphol2'
   config.download_dir = Rails.root.realpath.to_s + '/tmp/fargo/downloads'
   config.config_dir   = Rails.root.realpath.to_s + '/tmp/fargo/config'
+  config.websocket_host = '0.0.0.0'
 end
 
 Fargo.logger.level = ActiveSupport::BufferedLogger::DEBUG
