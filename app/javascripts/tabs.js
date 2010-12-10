71f8c3de6974588e1324b3552c416cd44b076f8c
Alex Crichton <alex@alexcrichton.com>
2010-12-10 18:27:23 -0500

    Using single quotes and bind along same things

--- app/javascripts/tabs.js
+++ app/javascripts/tabs.js
@@ -1,8 +1,7 @@
 //= require <jquery/ui>
 
 $(function() {
-  $('.tabs').tabs();
-  $(".tabs").bind("tabsshow", function(event, ui) { 
+  $('.tabs').tabs().bind('tabsshow', function(event, ui) {
     window.location.hash = ui.tab.hash;
   });
 });
