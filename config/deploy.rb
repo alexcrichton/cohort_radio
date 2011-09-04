e5c5d561a9521ef4da2ab36f37d9214528a3cb6b
Alex Crichton <alex@alexcrichton.com>
2011-09-04 14:31:55 -0400

    Streamline the deployment process

--- config/deploy.rb
+++ config/deploy.rb
@@ -23,13 +23,21 @@ namespace :deploy do
   end
   after 'deploy:setup', 'deploy:create_upload_dirs'
 
+  desc 'Link the shared private directory for files into place'
   task :link_private_dir do
     run "ln -nsf #{shared_path}/private #{latest_release}/private"
   end
   after 'deploy:update_code', 'deploy:link_private_dir'
+
+  desc 'Push the latest code to heroku'
+  task :push_to_heroku do
+    system 'git push heroku master'
+  end
+  before 'deploy:update_code', 'deploy:push_to_heroku'
 end
 
 namespace :foreman do
+  desc 'Start the foreman processes'
   task :start do
     run "cd #{current_release} && " \
         "nohup bundle exec foreman start " \
@@ -39,12 +47,15 @@ namespace :foreman do
           "--user #{user} &>> log/foreman.log &|"
   end
 
+  desc 'Stop the foreman processes'
   task :stop do
     run "cd #{current_release} && kill $(pgrep -f foreman)"
   end
 
+  desc 'Restart the foreman processes'
   task :restart do
     foreman.stop
     foreman.start
   end
+  after 'deploy:symlink', 'foreman:restart'
 end
