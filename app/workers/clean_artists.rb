d4d4706ab98d75f71db7248d52c7979bd9a8799b
Alex Crichton <alex@alexcrichton.com>
2011-09-19 00:37:36 -0400

    Link all files into a nice and neat directory every day

--- app/workers/clean_artists.rb
+++ app/workers/clean_artists.rb
@@ -11,6 +11,15 @@ class CleanArtists
       artist.destroy if artist.songs.size == 0 && artist.albums.size == 0
     end
 
+    if ENV['FARGO_LINK_DEST']
+      path = Pathname.new ENV['FARGO_LINK_DEST']
+      Song.all.each do |song|
+        song_path = path.join(song.artist_name, song.album_name, song.title)
+        song_path.dirname.mkpath
+        song_path.make_symlink song.audio.path
+      end
+    end
+
     CarrierWave.clean_cached_files!
   end
 
